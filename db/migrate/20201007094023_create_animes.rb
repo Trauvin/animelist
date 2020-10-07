88b9a85791d8d0dcf30c68632a8c5d8eda359bd1
Trauvin <mateusgalote@gmail.com>
2020-10-07 07:29:10 -0300

    init

---
+++ db/migrate/20201007094023_create_animes.rb
@@ -0,0 +1,11 @@
+class CreateAnimes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :animes do |t|
+      t.string :title
+      t.float :grade
+      t.text :resume
+
+      t.timestamps
+    end
+  end
+end
